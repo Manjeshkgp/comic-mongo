3cb32bf3f52c738dea6e6e06533f9d2eae642d0f
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-16 15:22:42 +0530

    validators added

--- src/routes/comic.routes.js
+++ src/routes/comic.routes.js
@@ -1,13 +1,21 @@
 const { Router } = require("express");
 const ComicController = require("../controllers/comic.controller");
+const ComicValidator = require("../validators/comic.validators");
+const { validateObjectId } = require("../validators/routes.validator");
 
 const ComicRouter = Router();
 
-ComicRouter.route("/").get(ComicController.getAll).post(ComicController.create);
+ComicRouter.route("/")
+  .get(ComicController.getAll)
+  .post(ComicValidator.validateCreateComic, ComicController.create);
 
 ComicRouter.route("/:id")
-  .get(ComicController.getById)
-  .patch(ComicController.update)
-  .delete(ComicController.delete);
+  .get(validateObjectId, ComicController.getById)
+  .patch(
+    validateObjectId,
+    ComicValidator.validateUpdateComic,
+    ComicController.update
+  )
+  .delete(validateObjectId, ComicController.delete);
 
 module.exports = ComicRouter;
---
+++ src/validators/comic.validators.js
@@ -0,0 +1,62 @@
+const Joi = require("joi");
+
+const createComicSchema = Joi.object({
+  name: Joi.string().required(),
+  author: Joi.string().required(),
+  yearOfPublication: Joi.number().integer().min(1000).max(9999).required(),
+  price: Joi.number().required(),
+  discount: Joi.number().optional(),
+  numberOfPages: Joi.number().required(),
+  condition: Joi.string().valid("new", "used", "pre-sale").required(),
+  description: Joi.string().optional(),
+});
+
+const validateCreateComic = (req, res, next) => {
+  const { error } = createComicSchema.validate(req.body, {
+    allowUnknown: false,
+  });
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+const updateComicSchema = Joi.object({
+  name: Joi.string().disallow(null).optional(),
+  author: Joi.string().disallow(null).optional(),
+  yearOfPublication: Joi.number()
+    .integer()
+    .min(1000)
+    .max(9999)
+    .disallow(null)
+    .optional(),
+  price: Joi.number().disallow(null).optional(),
+  discount: Joi.number().disallow(null).optional(),
+  numberOfPages: Joi.number().disallow(null).optional(),
+  condition: Joi.string()
+    .valid("new", "used", "pre-sale")
+    .disallow(null)
+    .optional(),
+  description: Joi.string().disallow(null).optional(),
+}).or(
+  "name",
+  "author",
+  "yearOfPublication",
+  "price",
+  "discount",
+  "numberOfPages",
+  "condition",
+  "description"
+);
+
+const validateUpdateComic = (req, res, next) => {
+  const { error } = updateComicSchema.validate(req.body, {
+    allowUnknown: false,
+  });
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+module.exports = { validateCreateComic, validateUpdateComic };
